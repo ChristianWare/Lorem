df537b597101a55040dcf61975e1160a753abf94
Chris <chris.ware.dev@gmail.com>
2021-08-29 17:27:36 -0700

    useState Functionality to generate text

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ import React, { useState } from "react";
 import data from'./data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Hello World')
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="text" name='amount' id='amount' value={count}
+          onChange={(e)=> setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article className="lorem-text">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, eius. </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, eius.</p>
+      </article>
+    </section>
   );
 }
 
