df0727abb0a8499a197bc896509993e944a5b2b2
Chris <chris.ware.dev@gmail.com>
2021-08-29 17:54:59 -0700

    handleSubmit Functionality/Complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import data from'./data';
+import data from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,24 +7,36 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello World')
-  }
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
 
   return (
     <section className='section-center'>
       <h3>Tired of boring lorem ipsum?</h3>
       <form className='lorem-form' onSubmit={handleSubmit}>
-        <label htmlFor="amount">
-          paragraphs:
-        </label>
-        <input type="text" name='amount' id='amount' value={count}
-          onChange={(e)=> setCount(e.target.value)}
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
         />
-        <button type='submit' className='btn'>Generate</button>
+        <button type='submit' className='btn'>
+          Generate
+        </button>
       </form>
-      <article className="lorem-text">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, eius. </p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, eius.</p>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
